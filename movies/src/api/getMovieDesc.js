55abea154afdd6654f535bb9e01d97adec631b5a
jevita_pearl <jevitapearl@gmail.com>
2026-02-08 23:37:06 +0530

    fixing routing

--- movies/src/api/getMovieDesc.js
+++ movies/src/api/getMovieDesc.js
@@ -2,7 +2,13 @@ export async function getMovieDesc({params}){
 
   try{
     const response = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${params.movieID}&plot=full`);
-    const data = response.json();
+    
+    if (response.status === 401) {
+      console.error("API Key is invalid or missing!");
+      return null;
+    }
+
+    const data = await response.json();
     return data;
   }
   catch(error){
--- movies/src/api/getMoviesData.js
+++ movies/src/api/getMoviesData.js
@@ -2,7 +2,13 @@ export async function getMoviesData(){
 
   try{
     const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=avengers&page=1`);
-    const data = response.json();
+
+    if (response.status === 401) {
+      console.error("API Key is invalid or missing!");
+      return null;
+    }
+
+    const data = await response.json();
     return data;
 
   }
