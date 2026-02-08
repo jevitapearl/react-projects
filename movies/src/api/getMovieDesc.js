a0ca55e1a304ef7eb5fc7c9e6d2407c9765f9cda
jevita_pearl <jevitapearl@gmail.com>
2026-02-08 23:16:54 +0530

    fixing routing

--- movies/src/api/getMovieDesc.js
+++ movies/src/api/getMovieDesc.js
@@ -1,7 +1,7 @@
 export async function getMovieDesc({params}){
 
   try{
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${params.movieID}&plot=full`);
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${params.movieID}&plot=full`);
     const data = response.json();
     return data;
   }
--- movies/src/api/getMoviesData.js
+++ movies/src/api/getMoviesData.js
@@ -1,7 +1,7 @@
 export async function getMoviesData(){
 
   try{
-    const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=avengers&page=1`);
+    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=avengers&page=1`);
     const data = response.json();
     return data;
 
